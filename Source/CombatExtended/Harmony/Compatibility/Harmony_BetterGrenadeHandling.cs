5d8480ca70f35e704958d7c14a98e7d7016cdaff
Safairette <71556532+Safairette@users.noreply.github.com>
2026-06-02 19:35:39 +0300

    Better Grenade Handling harmony patch

---
+++ Source/CombatExtended/Harmony/Compatibility/Harmony_BetterGrenadeHandling.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace CombatExtended.HarmonyCE.Compatibility;
+
+public class Harmony_BetterGrenadeHandling
+{
+    private static Type TypeOfBGHUtils
+    {
+        get
+        {
+            return AccessTools.TypeByName("BetterGrenadeHandling.BGHUtils");
+        }
+    }
+
+    [HarmonyPatch]
+    public static class Harmony_ShouldBeHitByEMP
+    {
+        public static bool Prepare()
+        {
+            return TypeOfBGHUtils != null;
+        }
+
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(TypeOfBGHUtils, "ShouldBeHitByEMP");
+        }
+
+        public static bool Prefix(Thing target, ref bool __result)
+        {
+            Pawn targetPawn = target as Pawn;
+            __result = targetPawn?.stances?.stunner?.StunFromEMP == false;
+            
+            return false;
+        }
+    }
+}
